a695eb7951a1786e4eeda93e489c154856bd4d72
Václav Dovrtěl <vaclav.dovrtel@gmail.com>
2016-04-07 16:50:39 +0200

    CODE: get all links of subresources

--- server/lib/Plack/App/RESTMy.pm
+++ server/lib/Plack/App/RESTMy.pm
@@ -21,6 +21,45 @@ sub new {
 	return $self;
 }
 
+sub returnResourcesLinks {
+	my ($self, $env, $rMap) = @_;
+
+	my $restMap = $rMap ? $rMap : $self->rmap->{resources};
+
+	my $links;
+	my $restNewMap = {};
+	$self->restMount('',$restMap,$restNewMap);
+	foreach my $resource (sort {($restNewMap->{$a}{class}||$restNewMap->{$a}{ref}) cmp ($restNewMap->{$b}{class}||$restNewMap->{$b}{ref})}  keys %{$restNewMap}) {
+		my $mod = $restNewMap->{$resource}{ref} || $restNewMap->{$resource}{class}||undef;
+		next unless $mod;
+		push @$links, {
+			href => $self->refToUrl($env, $mod, {}),
+			title => $mod,
+			rel => $mod,
+		}
+	}
+	return $links;
+}
+
+sub restMount {
+	my ($self, $uri,$rMap,$restNewMap) = @_;
+	my $staticEnv = $self->const->get("PlackEnv") ne "deployment"?"development":$self->const->get("PlackEnv");
+	foreach my $key (keys %{$rMap}) {
+		next if ( $key =~ /^\/\*$/ );
+		if (exists $rMap->{$key}{class} or exists $rMap->{$key}{ref} or exists $rMap->{$key}{resources} ){
+			my $stat = $uri."".$key;
+			if (exists $rMap->{$key}{class} or exists $rMap->{$key}{ref}){
+				$restNewMap->{$stat}{ref} = $rMap->{$key}{ref} if  $rMap->{$key}{ref};
+				$restNewMap->{$stat}{class} = $rMap->{$key}{class} if  $rMap->{$key}{class};
+				$restNewMap->{$stat}{unauthorized} = $rMap->{$key}{unauthorized} if $rMap->{$key}{unauthorized};
+			}
+			if(exists $rMap->{$key}{resources}){
+				$self->restMount($stat,$rMap->{$key}{resources},$restNewMap);
+			}
+		}
+	}
+}
+
 sub structToLinksByType {
 	my ($self, $rel, $base, $data) = @_;
 
