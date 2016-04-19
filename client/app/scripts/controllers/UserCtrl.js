a98c6daa4ef2bcfbb6ddbfd0844830dc2552cf1b
Michal Dovrtěl <m.dovrtel@gmail.com>
2016-04-20 00:18:05 +0200

    CODE: css update

--- client/app/scripts/controllers/UserCtrl.js
+++ client/app/scripts/controllers/UserCtrl.js
@@ -1,10 +1,26 @@
-angular.module('FitBoard').controller('UserCtrl', function($scope, $http) {
+angular.module('FitBoard').controller('UserCtrl', function($scope) {
 	'use strict';
 
 
 //dropdown controller
 	$scope.status = {
 	isopen: false
+	};
+
+// sex remote
+	$scope.sex = [
+    {value: 'male', text: 'male'},
+    {value: 'female', text: 'female'}
+  	];
+
+ // editBasic
+$scope.opened = {};
+
+ $scope.open = function($event, elementOpened) {
+		$event.preventDefault();
+		$event.stopPropagation();
+
+		$scope.opened[elementOpened] = !$scope.opened[elementOpened];
 	};
 
 //User
@@ -17,7 +33,7 @@ angular.module('FitBoard').controller('UserCtrl', function($scope, $http) {
 					gym: 'Fit Monster',
 					bDay: '06/06/1991',
 					password: 'michal',
-					sex: 'male',
+					sex: 'female',
 					category: 'elite',
 					shirt: 'm',
 					terms: 1,
@@ -27,14 +43,14 @@ angular.module('FitBoard').controller('UserCtrl', function($scope, $http) {
 					startFee: 0,
 					photo: '',
 					score: 0,
-					height: '175',
-					weight: '80',
-					yInC: '5',
-					cj: '90 kg',
-					snatch: '75 kg',
-					deadlift: '150 kg',
-					frontSquat: '150 kg',
-					backSquat: '160 kg',
+					height: 175,
+					weight: 80,
+					yInC: 5,
+					cj: 90,
+					snatch: 75,
+					deadlift: 150,
+					frontSquat: 150,
+					backSquat: 160,
 					fran: '3:14',
 					helen: '132',
 					grace: '2:15',
@@ -47,43 +63,43 @@ angular.module('FitBoard').controller('UserCtrl', function($scope, $http) {
 
 //my profile
 
-	$scope.saveBasic = function() {
-    // $scope.athlete already updated!
-    	return $http.post('/saveBasic', $scope.athlete).error(function(err) {
-      		if(err.field && err.msg) {
-        	// err like {field: "name", msg: "Server-side error for this username!"} 
-        		$scope.editableForm.$setError(err.field, err.msg);
-      		} else { 
-        	// unknown error
-        		$scope.editableForm.$setError('name', 'Unknown error!');
-      		}
-    	});
-  	};
+// 	$scope.saveBasic = function() {
+//     // $scope.athlete already updated!
+//     	return $http.post('/saveBasic', $scope.athlete).error(function(err) {
+//       		if(err.field && err.msg) {
+//         	// err like {field: "name", msg: "Server-side error for this username!"} 
+//         		$scope.editableForm.$setError(err.field, err.msg);
+//       		} else { 
+//         	// unknown error
+//         		$scope.editableForm.$setError('name', 'Unknown error!');
+//       		}
+//     	});
+//   	};
 
-  	$scope.savePass = function() {
-    // $scope.athlete already updated!
-    	return $http.post('/savePass', $scope.athlete).error(function(err) {
-      		if(err.field && err.msg) {
-        	// err like {field: "name", msg: "Server-side error for this username!"} 
-        		$scope.editableForm.$setError(err.field, err.msg);
-      		} else { 
-        	// unknown error
-        		$scope.editableForm.$setError('name', 'Unknown error!');
-      		}
-    	});
-  	};
+//   	$scope.savePass = function() {
+//     // $scope.athlete already updated!
+//     	return $http.post('/savePass', $scope.athlete).error(function(err) {
+//       		if(err.field && err.msg) {
+//         	// err like {field: "name", msg: "Server-side error for this username!"} 
+//         		$scope.editableForm.$setError(err.field, err.msg);
+//       		} else { 
+//         	// unknown error
+//         		$scope.editableForm.$setError('name', 'Unknown error!');
+//       		}
+//     	});
+//   	};
 
-  	// ---------------- mock $http requests --------------------
-angular.module('FitBoard').run(function($httpBackend) {
+//   	// ---------------- mock $http requests --------------------
+// angular.module('FitBoard').run(function($httpBackend) {
     
-  $httpBackend.whenPOST(/\/saveUser/).respond(function(method, url, data) {
-    data = angular.fromJson(data);
-    if(data.name === 'error') {
-      return [500, {field: 'name', msg: 'Server-side error for this username!'}]; 
-    } else {
-      return [200, {status: 'ok'}]; 
-    }
-  });
+//   $httpBackend.whenPOST(/\/saveUser/).respond(function(method, url, data) {
+//     data = angular.fromJson(data);
+//     if(data.name === 'error') {
+//       return [500, {field: 'name', msg: 'Server-side error for this username!'}]; 
+//     } else {
+//       return [200, {status: 'ok'}]; 
+//     }
+//   });
 
-});
+// 	});
 });
