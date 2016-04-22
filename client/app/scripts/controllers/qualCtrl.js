2f8b6551dbeda44b02e367eadeea4acfe61bb71f
Václav Dovrtěl <vaclav.dovrtel@gmail.com>
2016-04-22 10:29:21 +0200

    CODE: Submit qual video

--- client/app/scripts/controllers/qualCtrl.js
+++ client/app/scripts/controllers/qualCtrl.js
@@ -1,12 +1,88 @@
-angular.module('FitBoard').controller('qualCtrl', function($scope, $uibModal) {
+/* global qualvideoForm*/
+
+function clone(obj) {
+	'use strict';
+    if (null === obj || 'object' !== typeof obj){
+    	return obj;
+    }
+    var copy = obj.constructor();
+    for (var attr in obj) {
+        if (obj.hasOwnProperty(attr)){
+        	copy[attr] = obj[attr];
+        }
+    }
+    return copy;
+}
+
+angular.module('FitBoard').controller('qualCtrl', function($scope, $uibModal, Api) {
 	'use strict';
 
-	// $uibModal.open({
-	// 		animation: true,
-	// 		templateUrl: 'views/utilities/videoSubmitted.html',
-	// 		controller: 'ModalInstanceCtrl',
-	// 		size: 'm',
-	// 		backdrop: 'static',
-	// 		keyboard: false
-	// });
+	// Clean errors
+	$scope.errors = {};
+	$scope.errors.emailAlreadyExists = false; //400
+	$scope.errors.internal = false; //500
+	$scope.errors.unavailable = false; //503
+
+	// Set default
+	$scope.athlete = {};
+
+	$scope.setDefault = function(){
+		$scope.athlete = {
+			login: '',
+			points: 0,
+			video: '',
+		};
+	};
+	$scope.setDefault();
+
+	// Submit registration
+	$scope.submit = function() {
+		if ($scope.athlete.video && $scope.athlete.login) {
+			var athl = clone($scope.athlete);
+
+			// Clean errors
+			$scope.errors.emailError = false;
+			$scope.errors.emailAlreadyExists = false;
+			$scope.errors.badRequest = false;
+			$scope.errors.unavailable = false;
+			$scope.errors.internal =  false;
+
+			Api.qual('fitmonster2016',
+				athl,
+				function(resp){ // OK
+					if (resp){
+
+						// Show modal page
+						$uibModal.open({
+							animation: true,
+							templateUrl: 'views/utilities/videoSubmitted.html',
+							controller: 'ModalInstanceCtrl',
+							size: 'm',
+							backdrop: 'static',
+							keyboard: false
+						});
+//						$scope.setDefault();
+					}
+				},
+				function(resp){ //ERROR
+
+					if (resp.status === 400 && resp.data[0] === 'Bad email'){
+						qualvideoForm.$invalid = true;
+						$scope.errors.emailError = true;
+					}else if(resp.status === 400 && resp.data[0] === 'Qual for user exists'){
+						qualvideoForm.$invalid = true;
+						$scope.errors.emailAlreadyExists = true;
+					}else if(resp.status === 400 && resp.data[0] === 'Bad request'){
+						qualvideoForm.$invalid = true;
+						$scope.errors.badRequest = true;
+					}else if(resp.status === 503){
+						$scope.errors.unavailable = true;
+					}else{
+						$scope.errors.internal =  true;
+					}
+				}
+			);
+		}
+	};
+
 });
--- client/app/scripts/services/api.js
+++ client/app/scripts/services/api.js
@@ -7,6 +7,9 @@ angular.module('FitBoard')
 			register: function (data, callback, callbackErr) {
 				$http.post('/api/v1/auth/register', data).then(callback, callbackErr);
 			},
+			qual: function (compet, data, callback, callbackErr) {
+				$http.post('/api/v1/competition/'+compet+'/qual', data).then(callback, callbackErr);
+			},
 			getRoster: function (compet, callback, callbackErr) {
 				$http.get('/api/v1/competition/'+compet+'/roster').then(callback, callbackErr);
 			},
