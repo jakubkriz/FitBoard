a3d34bc1d1f01e7c1407910a69f24793084f790c
Vaclav Dovrtel <vasek@localhost.localdomain>
2016-05-03 14:26:13 +0200

    CODE: Fixed typo

--- client/app/scripts/directives/aDisable.js
+++ client/app/scripts/directives/aDisable.js
@@ -1,13 +1,13 @@
 angular.module('FitBoard').directive('aDisabled', function() {
 	'use strict';
     return {
-        compile: function(tElement, tAttrs, transclude) {
+        compile: function(tElement, tAttrs) {
             //Disable ngClick
-            tAttrs['ngClick'] = '!('+tAttrs['aDisabled']+') && ('+tAttrs['ngClick']+')';
+            tAttrs.ngClick = '!('+tAttrs.aDisabled+') && ('+tAttrs.ngClick+')';
 
             //Toggle 'disabled' to class when aDisabled becomes true
             return function (scope, iElement, iAttrs) {
-                scope.$watch(iAttrs['aDisabled'], function(newValue) {
+                scope.$watch(iAttrs.aDisabled, function(newValue) {
                     if (newValue !== undefined) {
                         iElement.toggleClass('disabled', newValue);
                     }
@@ -15,7 +15,7 @@ angular.module('FitBoard').directive('aDisabled', function() {
 
                 //Disable href on click
                 iElement.on('click', function(e) {
-                    if (scope.$eval(iAttrs['aDisabled'])) {
+                    if (scope.$eval(iAttrs.aDisabled)) {
                         e.preventDefault();
                     }
                 });
