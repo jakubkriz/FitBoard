3ad507643776faee1b8472f070c8880980ac2109
Vaclav Dovrtel <vasek@localhost.localdomain>
2016-05-17 17:30:55 +0200

    CODE: Added all keys to table

--- server/lib/Rest/HtmlVis/Table.pm
+++ server/lib/Rest/HtmlVis/Table.pm
@@ -1,4 +1,4 @@
-package Rest::HtmlVis::Users;
+package Rest::HtmlVis::Table;
 
 use strict;
 use warnings;
@@ -16,15 +16,20 @@ sub head {
 	my ($self, $local) = @_;
 
 	my $static = $self->baseurl;
+  my $title = $self->{env}{'REST.class'};
 	return '
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Users</title>
+	<title>'.$title.'</title>
 
   <link rel="stylesheet" type="text/css" href="/localstatic/datatables/datatables.min.css"/>
+  <link rel="stylesheet" type="text/css" href="/localstatic/datatables/Buttons-1.1.2/css/buttons.dataTables.min.css"/>
    
   <script type="text/javascript" src="/localstatic/datatables/datatables.min.js"></script>
+  <script type="text/javascript" src="/localstatic/datatables/Buttons-1.1.2/js/dataTables.buttons.min.js"></script>
+  <script type="text/javascript" src="/localstatic/datatables/Buttons-1.1.2/js/buttons.html5.min.js"></script>
+  <script type="text/javascript" src="/localstatic/datatables/jszip.min.js"></script>
 
 	<!-- Custom styles for this template -->
 	<style>
@@ -32,7 +37,15 @@ sub head {
 	</style>
   <script>
     $(document).ready(function() {
-      $(\'#users\').DataTable();
+      var table = $(\'#users\').DataTable({
+        buttons: [
+            \'copyHtml5\',
+            \'excelHtml5\',
+            \'csvHtml5\',
+        ]
+      });
+      table.buttons().container()
+      .appendTo( $(\'.col-sm-6:eq(0)\', table.table().container() ) );
     } );
   </script>
 	'
